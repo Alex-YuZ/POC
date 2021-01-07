dc0830864f70252e33e74f683f3f8fd6be99dcd5
AlejandroSanz <alexyuzhu@gmail.com>
2021-01-07 14:55:42 +0800

    docs: reconstruct find_anagrams docstring

--- python_basics_and_intermediates/anagrams/anagrams.py
+++ python_basics_and_intermediates/anagrams/anagrams.py
@@ -2,11 +2,15 @@ from wordsets import english_words, english_words_small
 
 
 def find_anagrams(letters, words):
-    """Find a collection of anagrams of given letters from a given word bank.
-
-    :param letters: The letters from which to form anagrams.
-    :param words: A set of lowercase, alphabetic English words in a word bank.
-    :return: A set of anagrams of the given letters found in the word bank.
+    """
+    Find a collection of anagrams of given letters from a given word bank.
+    
+    Parameter:
+        letters -> string : The letters from which to form anagrams.
+        words -> iterables : A set of lowercase, alphabetic English words in a word bank.
+        
+    Returns -> iterables: A set of anagrams of the given letters found in the word bank.
+    
     """
     lookup = {}
     for word in words:
